d0dc012612aa07fb1ae1b6bd5dd714560deab6d0
reid <reidbras@gmail.com>
2021-07-31 20:40:28 -0500

    Adding the spelled day of the week to the date and comments

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+// Loop every second getting the time and present a date in
+// the format 'DDD HH : MM : SS AM/PM'
 function currentTime() {
     let date = new Date();
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
     let ampm = updateAmPm(hour);
+    let day = date.getDay();
+    let daySpelled = updateDay(day);
     hour = updateHour(hour);
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + ampm;
+    document.getElementById("clock").innerText = daySpelled + " " + hour + " : " + min + " : " + sec + " " + ampm;
     let t = setTimeout(function () {
         currentTime()
     }, 1000);
 }
 
+// Add a leading zero to the hour min and sec
 function updateTime(k) {
     if (k < 10) {
         return "0" + k;
@@ -23,6 +28,7 @@ function updateTime(k) {
     }
 }
 
+// Return a AM PM Noon or Midnight indication for a given hour and minutes
 function updateAmPm(hour, min) {
     let ampm;
 
@@ -40,12 +46,35 @@ function updateAmPm(hour, min) {
     return ampm;
 }
 
+// Return the hour as 1 - 12 instead of 0 - 23
 function updateHour(hour) {
 
     if (hour > 12) {
-        hour = hour - 12;
+        return hour - 12;
     }
-    return hour;
+    else if(hour == 0) {
+        return 12;
+    }
+}
+
+// Returned the spelled day of the week
+function updateDay(day) {
+    switch(day) {
+        case 0:
+            return "SUN";
+        case 1:
+            return "MON";
+        case 2:
+            return "TUE";
+        case 3:
+            return "WED";
+        case 4:
+            return "THU";
+        case 5:
+            return "FRI";
+        case 6:
+            return "SAT";
+    }    
 }
 
 currentTime();
